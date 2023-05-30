8f6c7336a39c395b7a73a9ced2c63477ce2092c8
yizusdies <yizusdies@yahoo.com>
2023-05-30 12:22:31 -0600

    primer programa python

---
+++ cuentaletras.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 11:40:05 2023
+
+@author: CIBERESCUELAS
+"""
+
+cadena=input("Escribe una palabra\n")
+for letra in cadena:
+    if letra== "a":
+        print(letra)
